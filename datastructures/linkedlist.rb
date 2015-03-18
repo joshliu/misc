367e3fd6ec4b17192239e5ce6f34b93ccf8a88fe
Joshua Liu <joliu@htps.us>
2015-03-18 14:52:13 -0400

    fixed the sorts

--- datastructures/linkedlist.rb
+++ datastructures/linkedlist.rb
@@ -1,6 +1,70 @@
 require 'pry'
 
 class Node
-  
+	attr_accessor :value, :next, :previous
+  def initialize(value)
+  	@value = value
+  end
+
+  def append(node)
+  	if node.class == Fixnum
+  		node = Node.new(node)
+  	end
+  	node.previous = self.last()
+  	self.last().next = node
+  end
+
+  def last()
+  	return self if self.next == nil
+  	node = self.next.last()
+  end
+
+  def first()
+  	return self if self.previous == nil
+  	node = self.previous.first()
+  end
+
+  def to_s()
+  	return self.value.to_s
+  end
+
+  def at_index(int)
+  	node = self.first()
+  	i = 0
+  	while i < int-1
+  		node = node.next
+  		i += 1
+  	end
+  	return node
+  end
+
+  def list()
+  	start = self.first()
+  	array = []
+  	while start.next != nil
+  		array << start.value
+  		start = start.next
+  	end
+  	return array
+  end
+
+  def delete()
+  	if node.first == node
+  		if node.next == nil
+  			return
+  		end
+  		node.next.previous = nil
+  		return
+  	end
+  	if node.last == node
+  		node.previous.next = nil
+  		return
+  	end
+  	node.next.previous = node.previous
+  	node.previous.next = node.next
+  end
+
+
+end
 
 binding.pry
--- sorts/mergesort.rb
+++ sorts/mergesort.rb
@@ -1,44 +1,44 @@
 require 'pry'
 
 def merge_sort(arr)
-	  if arr.length <= 1
-		      return arr
-		        end
+  if arr.length <= 1
+    return arr
+  end
 
-	    left,right = [],[]
-	      mid_index = arr.length/2
+  left,right = [],[]
+  mid_index = arr.length/2
 
-	        arr[0...mid_index].each { |n| left << n }
-		  arr[mid_index..arr.length].each { |n| right << n }
+  arr[0...mid_index].each { |n| left << n }
+  arr[mid_index..arr.length].each { |n| right << n }
 
-		    left = merge_sort(left)
-		      right = merge_sort(right)
+  left = merge_sort(left)
+  right = merge_sort(right)
 
-		        return merge(left,right)
+  return merge(left,right)
 end
 
 def merge(left,right)
-	  result = []
+  result = []
 
-	    while left.length > 0 || right.length > 0
-		        if left.length > 0 && right.length > 0
-				      if left[0] <= right[0]
-					              result << left[0]
-						              left.shift
-							            else
-									            result << right[0]
-										            right.shift
-											          end
-				          elsif left.length > 0
-						        result << left[0]
-							      left.shift
-							          else
-									        result << right[0]
-										      right.shift
-										          end
-			  end
+  while left.length > 0 || right.length > 0
+    if left.length > 0 && right.length > 0
+      if left[0] <= right[0]
+        result << left[0]
+        left.shift
+      else
+        result << right[0]
+        right.shift
+      end
+    elsif left.length > 0
+      result << left[0]
+      left.shift
+    else
+      result << right[0]
+      right.shift
+    end
+  end
 
-	      return result
+  return result
 end
 
 binding.pry
