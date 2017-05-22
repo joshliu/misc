a119ce60083c14bdfc79c630c4e953aa484e1d7e
Joshua Liu <joshliu6830@gmail.com>
2017-05-22 16:15:33 -0400

    Add perl molar mass

--- scrapes/atomic.rb
+++ scrapes/atomic.rb
@@ -1,8 +1,12 @@
 require 'pry'
 require 'open-uri'
-require 'active_record'
 require 'nokogiri'
 
 url = 'http://www.csudh.edu/oliver/chemdata/atmass.htm'
 
 doc = Nokogiri::HTML(open(url))
+
+table = doc.css("table")[0]
+table.css("tr").each do |row|
+	puts "'#{row.children[3].text}' => #{row.children[7].text.gsub("(","").gsub(")","").to_f},"
+end
