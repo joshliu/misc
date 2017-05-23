61936c8cba4407265e3024811663ccf79ec2f651
Joshua Liu <joshliu6830@gmail.com>
2017-05-22 22:06:56 -0400

    molar mass stuff lol

--- scrapes/atomic.rb
+++ scrapes/atomic.rb
@@ -6,7 +6,13 @@ url = 'http://www.csudh.edu/oliver/chemdata/atmass.htm'
 
 doc = Nokogiri::HTML(open(url))
 
+out_file = File.new("elements.yml", "w")
+
+out_file.puts("---")
 table = doc.css("table")[0]
+
+i = 0
 table.css("tr").each do |row|
-	puts "'#{row.children[3].text}' => #{row.children[7].text.gsub("(","").gsub(")","").to_f},"
+	out_file.puts "[\"#{row.children[3].text}\", \"#{row.children[7].text.gsub("(","").gsub(")","").to_f}\"]" if i != 0
+  i += 1
 end
